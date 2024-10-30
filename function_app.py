44535ab51f6e7a2d53e71118462c003747e25282
Anthony Altieri <anthonyaltieri0@gmail.com>
2024-10-30 09:54:26 -0400

    temperature is being received

--- function_app.py
+++ function_app.py
@@ -1,10 +1,15 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp()
 
 @app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="mqtt-messages",
-                               connection="mqttrouting_listenconnectionstring_EVENTHUB") 
+                               connection="HUB_INSTANCE_CONNECTION_STRING") 
 def eventhub_trigger(azeventhub: func.EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s',
                 azeventhub.get_body().decode('utf-8'))
+    event_data = json.loads(azeventhub.get_body().decode('utf-8'))
+    temp = event_data['data']['temperature']
+    logging.info(f'Temperature is {temp} degrees F')
+    
